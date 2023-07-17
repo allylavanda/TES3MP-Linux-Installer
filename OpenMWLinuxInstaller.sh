1f3fcb7de916c1d089c847fdbcf04599f3cd0ac2
Daniel <allylavanda@aol.com>
2023-07-17 16:22:05 -0400

    Update OpenMWLinuxInstaller.sh

--- OpenMWLinuxInstaller.sh
+++ OpenMWLinuxInstaller.sh
@@ -16,6 +16,7 @@ wget https://github.com/allylavanda/TES3MP-Linux-Installer/blob/main/microwave.p
 mv microwave.png?raw=true microwave.png
 mv microwave.png ~/.local/share/icons/microwave.png
 echo -e "[Desktop Entry]\nEncoding=UTF-8\nVersion=1.0\nType=Application\nTerminal=false\nExec=$cwd/startmw.sh\nName=TES3MP\nComment=OpenMW TES3MP+Server Launcher\nIcon=~/.local/share/icons/microwave.png" > ~/.local/share/applications/TES3MP.desktop
+echo -e "[Desktop Entry]\nEncoding=UTF-8\nVersion=1.0\nType=Application\nTerminal=false\nExec=$cwd/tes3mp\nName=TES3MP\nComment=OpenMW TES3MP Launcher\nIcon=~/.local/share/icons/microwave.png" > ~/.local/share/applications/TES3MP.desktop
 chmod +x ~/.local/share/applications/TES3MP.desktop
 
 sh openmw-wizard
